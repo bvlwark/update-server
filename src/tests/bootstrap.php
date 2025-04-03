4f59332158450fc50f66fe448595ff44022ddd5b
Drazen Bebic <drazen.bebic@outlook.com>
2025-04-03 21:45:13 +0200

    chore: add php unit test dependency & basic config

---
+++ src/tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+// Require function files.
+require_once __DIR__ . '/../functions/core-functions.php';
+require_once __DIR__ . '/../functions/item-functions.php';
+require_once __DIR__ . '/../functions/item-version-functions.php';
+require_once __DIR__ . '/../functions/template-functions.php';
